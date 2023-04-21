fe42357d702680c58e1fa9e7ca2b1e6183487f69
rubi <naiman.music.info@gmail.com>
2023-04-21 22:06:11 +0200

    readme

--- src/example.py
+++ src/example.py
@@ -4,7 +4,7 @@ julia_module = pylia.Julia("./example.jl")
 
 my_args = [2, 3, 5]
 
-# julia_sum =julia_module.call_func("add", my_args)
-julia_functions = julia_module.get_funcs()
+julia_sum = julia_module.call_func("add", my_args)
+julia_prod = julia_module.call_func("multiply", my_args)
 
-print(julia_functions)
+print(julia_sum, julia_prod)
