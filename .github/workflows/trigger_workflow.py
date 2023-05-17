6f0d09000a139f3fe2cb325e24e00d485dc42c5d
Stefan Arsic <55553527+WoodyXP@users.noreply.github.com>
2023-05-17 10:51:54 +0200

    Created a unit test and extended some workflows

---
+++ .github/workflows/trigger_workflow.py
@@ -0,0 +1,40 @@
+import requests
+import os
+
+def trigger_workflow():
+
+    # Set the required information for the API request
+    repository = "https://github.com/WoodyXP/Pyjulia"
+    personal_token = os.getenv("GITHUB_TOKEN")
+    personal_token = "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"
+    workflow_file = "downstream.yml"
+
+    # Construct the API endpoint URL
+    api_url = f"https://api.github.com/repos/{repository}/actions/workflows/{workflow_file}/dispatches"
+
+    # Set the request headers
+    headers = {
+        "Authorization": f"Bearer {personal_token}",
+        "Accept": "application/vnd.github.v3+json"
+    }
+
+    # Define the request payload
+    payload = {
+        "ref": "main",
+        "inputs": {
+            "param1": "value1",
+            "param2": "value2"
+        }
+    }
+
+    # Send the API request
+    response = requests.post(api_url, headers=headers, json=payload)
+
+    # Check the response status code
+    if response.status_code == 204:
+        print("Downstream workflow triggered successfully.")
+    else:
+        print("Failed to trigger downstream workflow.")
+
+# Trigger the downstream workflow
+trigger_workflow()
---
+++ tests/pyjulia_test.py
@@ -0,0 +1,15 @@
+import unittest
+import pyjulia
+
+class TestPyJulia(unittest.TestCase):
+    def test_pyjulia_functions(self):
+        julia_module = pyjulia.Pyjulia("./example.jl")
+        test_args = [2, 3, 4]
+        
+        
+        sum_func = julia_module.call_func("add", test_args)
+
+        self.assertEqual(sum_func, 9)
+
+if __name__ == "__main__":
+    unittest.main() 
